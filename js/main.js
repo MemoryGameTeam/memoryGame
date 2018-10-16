a6fe7f6dce50a4df66b093d875f991a6bd0f2887
monahaleem <43965429+monahaleem@users.noreply.github.com>
2018-10-16 13:16:43 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var memoryDAta= new XMLHttpRequest();
 memoryDAta.open('GET','../data.json');
 memoryDAta.onload=function(){
     var data=JSON.parse(memoryDAta.responseText);
-    console.log(data[0].url);
+    console.log(data);
 };
 memoryDAta.send();
