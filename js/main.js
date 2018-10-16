50f73d45cb56e885644d55b2c40e75b24b442a4d
monahaleem <43965429+monahaleem@users.noreply.github.com>
2018-10-16 12:39:36 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,5 +2,6 @@ var memoryDAta= new XMLHttpRequest();
 memoryDAta.open('GET','../data.json');
 memoryDAta.onload=function(){
     var data=JSON.parse(memoryDAta.responseText);
+    console.log(data[0].url);
 };
 memoryDAta.send();
