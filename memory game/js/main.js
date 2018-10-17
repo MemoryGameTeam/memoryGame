0b4bd0fa28bd7fb820b47c0324ebcf8dce77cf5e
monahaleem <43965429+monahaleem@users.noreply.github.com>
2018-10-17 21:53:53 +0200

    Add files via upload

--- memory game/js/main.js
+++ memory game/js/main.js
@@ -9,10 +9,8 @@ setImages(gameData);
 };
 
 memoryDAta.send();
-countClicks();
 // function put json data in html
 function setImages(data){
-
 
     for(i=0;i<data.length; i++)
     {
@@ -21,33 +19,76 @@ function setImages(data){
         imgBlocks[i].alt=data[i].alt;        
     }  
 
-
-    
-   
 }
-function countClicks(){
-    
- for(i=0;i<imgBlocks.length;i++){
-document.getElementsByClassName("image")[i].addEventListener("click",function(e){
-    count++;clicks++;
-         document.getElementById("counte").innerHTML="clicks"+count;
-         if(clicks<3){
-        e.target.style.opacity="0";
-        console.log(e.srcElement.parentElement.firstElementChild.name);
-    
-        
-         }
-        
-        
+for(i=0;i<imgBlocks.length;i++){
+    document.getElementsByClassName("overlay")[i].addEventListener("click",countClicks);
+    // if(document.getElementById("unclick").onclick){
+     
+    // removeEventListener("click",countClicks);
+    // }
+}
+function countClicks(e){
+
+   if(e){
+
        
-})
+    if(count%2==1){
+        clicks--;
+         document.getElementById("counte").innerHTML="clicks"+clicks;
+        }
+    
+         if(count<3){
+        e.target.style.opacity="0"; 
+        
+         }  
+   
+   
+        count++;
+        if(count==1){
+            console.log(e);
+            firstclick=e.srcElement.parentElement.firstElementChild.name;
+            console.log(firstclick+"  " +count);
+            removediv1=e.srcElement.parentElement;
+            resetdiv1=e.srcElement;
+            
+        }
+        else if(count==2){
+            console.log(e);
+            secondclick=e.srcElement.parentElement.firstElementChild.name;
+            console.log(secondclick+"  " +count);
+            removediv=e.srcElement.parentElement;
+            resetdiv=e.srcElement;
+            console.log(resetdiv);
+            if(firstclick==secondclick){
+                count=0;
+                score++;
+                 setTimeout(function(){
+                removediv.style.visibility="hidden";
+                removediv1.style.visibility="hidden";
+                console.log("match");},200)
+                document.getElementById("score").innerHTML=score;
+                 
+            }
+                else{
+                    
+                    setTimeout(function(){                 
+                    resetdiv.style.opacity="1";
+                    resetdiv1.style.opacity="1";},200);
+                    count=0;
+                   
+                }
+        }
+        else if(count>2){
+            count=0;
+            console.log(e.srcElement.parentElement.firstElementChild.name+"  " +count);
+        }
+    
+
     }
 }
 
-
-
-
-
+     
+    
   let count=0;
-  let clicks=0;
-
+  let clicks=30;
+  let score=0;
