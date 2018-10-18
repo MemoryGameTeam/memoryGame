9174d90a3f0242eeb6ed373c226dfb069037b08e
monahaleem <43965429+monahaleem@users.noreply.github.com>
2018-10-18 17:15:01 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,73 +1,60 @@
-load();
-let imgBlocks=document.getElementsByClassName("blocks");
-// let overlayBlocks=document.getElementById("overlay");
+load(); //  load function with start page
+let imgBlocks=document.getElementsByClassName("blocks");// catch memory blocks to full
+
 // get jason data
-function load(){
-let memoryDAta= new XMLHttpRequest();
-memoryDAta.open('GET','../data.json');
-memoryDAta.onload =function(){
-     gameData=JSON.parse(memoryDAta.responseText);
-setImages(gameData);
+function load(){ //load function start with page reload 
+let memoryDAta= new XMLHttpRequest(); // ajax request
+memoryDAta.open('GET','../data.json'); //getjson file
+memoryDAta.onload =function(){  //convert json to object 
+     gameData=JSON.parse(memoryDAta.responseText); 
+     gameData.sort (function(){
+        return .5-Math.random();
+   });
+setImages(gameData); 
 };
 memoryDAta.send();
-startTimer();
 }
-
-
 // function put json data in html
 function setImages(data){
-
     for(i=0;i<data.length; i++)
     {
-        imgBlocks[i].src = data[i].URL; 
+        imgBlocks[i].src = data[i].URL;  
         imgBlocks[i].name=data[i].name;
         imgBlocks[i].id=data[i].id;
         imgBlocks[i].alt=data[i].alt;        
     }  
-
 }
 for(i=0;i<imgBlocks.length;i++){
-    document.getElementsByClassName("overlay")[i].addEventListener("click",countClicks);
-    // if(document.getElementById("unclick").onclick){
-     
-    // removeEventListener("click",countClicks);
-    // }
+ document.getElementsByClassName("overlay")[i].addEventListener("click",countClicks);
 }
+//blocks click event
 function countClicks(e){ 
-    allCount++;  
-if(allCount<=50){
-    if(count%2==1){
-        clicks--;
-         document.getElementById("counte").innerHTML="clicks"+clicks;
+     //click block event function
+    startTimer();// start timer to show spend time
+if(clicks<=50){ //max clicks users can do
+    if(count%2==1){ // calculate clicks after 2 clicks
+        clicks--; //minues number you should click
+         document.getElementById("counte").innerHTML="clicks"+clicks; //preview clicks in html
+        
         }
     
          if(count<3){
-        e.target.style.opacity="0"; 
-        // if(count==1){
-        // e.srcElement.parentElement.firstElementChild.id="id1";
-        // }
-        // else if(count==2){
-        //     e.srcElement.parentElement.firstElementChild.id="id2";
-
-        // }
-        // else{
-        //     e.srcElement.parentElement.firstElementChild.id="";
-        // }
-        
+        e.target.style.opacity="0";  // condition remove overlay
+     
          }  
-   
-   
-        count++;
+        count++; // increase count by 1 with every click
+
+        //condition with first click take id and name to be checked again
         if(count==1){
-        
             firstclick=e.srcElement.parentElement.firstElementChild.name;            
             firstClickId=e.srcElement.parentElement.firstElementChild.id;
-                     console.log(firstclick+"  "+firstClickId +count);
+            console.log(firstclick+"  "+firstClickId +count);
             removediv1=e.srcElement.parentElement;
             resetdiv1=e.srcElement;
             stopdiv1=e.srcElement.parentElement.firstElementChild;
             
         }
+        //condition with second click take id and name to be checked again
         else if(count==2){
             secondClickId=e.srcElement.parentElement.firstElementChild.id;
             secondclick=e.srcElement.parentElement.firstElementChild.name;
@@ -76,72 +63,83 @@ if(allCount<=50){
             resetdiv=e.srcElement;
             stopdiv=e.srcElement.parentElement.firstElementChild;
             console.log(resetdiv);
+            //condition if id and name match condition hide blocks
             if((firstclick==secondclick) && (firstClickId!=secondClickId)){
                 count=0;
                 score++;
                  setTimeout(function(){
-                removediv.style.visibility="hidden";
-                removediv1.style.visibility="hidden";
+                    stopdiv.removeEventListener("click",countClicks);
+                    stopdiv.style.opacity=".1";
+                    stopdiv1.style.opacity=".1";
             },200)
                 document.getElementById("score").innerHTML=score;
+                if(score=8){
+                    alert("congratulation suceesed click ok to reset");
+                    setImages(gameData);
+                    load();
+                    countClicks();
+                    
+                }
                  
             }
+                //condition if id and name doesnot match condition reset blocks
                 else{
-                   if(firstclick!=secondclick)
+                 
+                   if(firstclick!=secondclick){
                     setTimeout(function(){                 
                     resetdiv.style.opacity="1";
                     resetdiv1.style.opacity="1";},200);
-                    count=0;
-                    if(firstClickId==secondClickId){
-
-                    }
-                    
+                    count=0;}
+                        //condition to remove clicks of an open image
+                        if(firstClickId==secondClickId){                    
                         console.log( e.srcElement.parentElement.firstElementChild.id);
                         stopdiv.removeEventListener("click",countClicks);
                         count=1;
                     }
                    
                 
-        }
-        else if(count>2){
-            count=0;
+        }} //end of coundation count==2
 
+        //condition to reset count if it more than 2 clicks
+        else if(count>2){ 
+            count=0;
             console.log(e.srcElement.parentElement.firstElementChild.name+"  " +count);
         }
     
 
-    }
+    // if user take all avalible clicks start again
     else{
-        alert("lost press ok to start again");
+        alert("lose you use all your trys clicks press ok to start again");
         load();
         let count=0;
-        let clicks=30;
+        let clicks=50;
         let score=0;
       let allCount=0;
         
     }
-}
-var m = 0;
-var s = 0;
+} //end check if clicks less 50
 
+} // end event function
+var minutes = 0;
+var seconds = 0;
+
+
+// clock with start game will calling
 function startTimer() {
-    setInterval(pomodoro, 1000);
+    setInterval(timer, 1000);
 }
-function pomodoro() {
-    s++;
-    if (s == 60) {
-        m += 1;
-        s = 0;
+function timer() {
+    seconds++;
+    if (seconds == 60) {
+        minutes += 1;
+        seconds = 0;
        
     }
-    document.getElementById("timer").innerHTML = m + " :"+s;
+    document.getElementById("timer").innerHTML = minutes + " :"+seconds;
  
    
 }
-
-     
-    
+//variable defination   
   let count=0;
-  let clicks=30;
+  let clicks=50;
   let score=0;
-let allCount=0;
